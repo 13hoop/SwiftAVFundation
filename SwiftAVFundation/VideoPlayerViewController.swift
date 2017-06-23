c00af277315c9ba40ea5a81512978383924aa186
YongRen <zhanxin13@gmail.com>
2017-06-23 23:11:16 +0800

    add test video url

--- SwiftAVFundation/VideoPlayerViewController.swift
+++ SwiftAVFundation/VideoPlayerViewController.swift
@@ -11,11 +11,18 @@ import AVFoundation
 class VideoPlayerViewController: UIViewController {
   let avPlayer = AVPlayer()
   var avPlayerLayer: AVPlayerLayer!
-  let movieUrl: URL = URL(string: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")!
+//  let movieUrl: URL = URL(string: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")!
+//  http://baobab.wdjcdn.com/1456317490140jiyiyuetai_x264.mp4
+  let movieUrl: URL = URL(string: "http://baobab.wdjcdn.com/1456317490140jiyiyuetai_x264.mp4")!
   
   var timeObserver: Any!
   let invisibleButton = UIButton()
   let timeRemainingLabel = UILabel()
+  let loadingIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+  let seekSlider = UISlider()
+  var playerRateBeforeSeek: Float = 0
+
+  var playbackLikelyToKeepUpContext = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,6 +33,18 @@ class VideoPlayerViewController: UIViewController {
     view.addSubview(invisibleButton)
     invisibleButton.addTarget(self, action: #selector(invisibleButtonTapped),for: .touchUpInside)
     
+    view.addSubview(seekSlider)
+    seekSlider.addTarget(self, action: #selector(sliderBeganTracking),
+                         for: .touchDown)
+    seekSlider.addTarget(self, action: #selector(sliderEndedTracking),
+                         for: [.touchUpInside, .touchUpOutside])
+    seekSlider.addTarget(self, action: #selector(sliderValueChanged),
+                         for: .valueChanged)
+    
+    loadingIndicatorView.hidesWhenStopped = true
+    view.addSubview(loadingIndicatorView)
+    avPlayer.addObserver(self, forKeyPath: "currentItem.playbackLikelyToKeepUp", options: .new, context: &playbackLikelyToKeepUpContext)
+
     timeRemainingLabel.textColor = .white
     view.addSubview(timeRemainingLabel)
     
@@ -48,6 +67,18 @@ class VideoPlayerViewController: UIViewController {
 //    }
   }
   
+  
+  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    print("\(context!)===========")
+    if context == &playbackLikelyToKeepUpContext {
+      if avPlayer.currentItem!.isPlaybackLikelyToKeepUp {
+        loadingIndicatorView.stopAnimating()
+      } else {
+        loadingIndicatorView.startAnimating()
+      }
+    }
+  }
+  
   private func updateTimeLabel(elapsedTime: Float64, duration: Float64) {
     let timeRemaining: Float64 = CMTimeGetSeconds(avPlayer.currentItem!.duration) - elapsedTime
     timeRemainingLabel.text = String(format: "%02d:%02d", ((lround(timeRemaining) / 60) % 60), lround(timeRemaining) % 60)
@@ -55,6 +86,8 @@ class VideoPlayerViewController: UIViewController {
   
   deinit {
     avPlayer.removeTimeObserver(timeObserver)
+    avPlayer.removeObserver(self, forKeyPath: "currentItem.playbackLikelyToKeepUp")
+
   }
   
   func invisibleButtonTapped(sender: UIButton) {
@@ -64,6 +97,35 @@ class VideoPlayerViewController: UIViewController {
     } else {
       avPlayer.play()
     }
+  }
+  
+  func sliderBeganTracking(slider: UISlider) {
+    playerRateBeforeSeek = avPlayer.rate
+    avPlayer.pause()
+  }
+  
+  func sliderEndedTracking(slider: UISlider) {
+    let videoDuration = CMTimeGetSeconds(avPlayer.currentItem!.duration)
+    let elapsedTime: Float64 = videoDuration * Float64(seekSlider.value)
+    updateTimeLabel(elapsedTime: elapsedTime, duration: videoDuration)
+    
+    avPlayer.seek(to: CMTimeMakeWithSeconds(elapsedTime, 100)) { (completed: Bool) -> Void in
+      if self.playerRateBeforeSeek > 0 {
+        self.avPlayer.play()
+      }
+    }
+  }
+  
+  func sliderValueChanged(slider: UISlider) {
+    let videoDuration = CMTimeGetSeconds(avPlayer.currentItem!.duration)
+    let elapsedTime: Float64 = videoDuration * Float64(seekSlider.value)
+    updateTimeLabel(elapsedTime: elapsedTime, duration: videoDuration)
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    loadingIndicatorView.startAnimating()
+
   }
   
   override func viewDidAppear(_ animated: Bool) {
@@ -79,6 +141,10 @@ class VideoPlayerViewController: UIViewController {
     let controlsHeight: CGFloat = 30
     let controlsY: CGFloat = view.bounds.size.height - controlsHeight
     timeRemainingLabel.frame = CGRect(x: 5, y: controlsY, width: 60, height: controlsHeight)
+    seekSlider.frame = CGRect(x: timeRemainingLabel.frame.origin.x + timeRemainingLabel.bounds.size.width,
+                              y: controlsY, width: view.bounds.size.width - timeRemainingLabel.bounds.size.width - 5, height: controlsHeight)
+    loadingIndicatorView.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+
   }
   
   override var shouldAutorotate: Bool {
